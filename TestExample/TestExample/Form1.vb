d97c7797713305ab1da96b640efe604a916a5c6f
mshonk <mshonk2@gmail.com>
2016-06-15 13:35:41 -0400

    MessageBox Comment Altered
    Changed to "hi how are you."

--- TestExample/TestExample/Form1.vb
+++ TestExample/TestExample/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("hi")
+        MessageBox.Show("hi how are you")
     End Sub
 End Class
