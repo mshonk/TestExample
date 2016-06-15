a99017ba8ee5053949554d2fcbc356586b44f01d
mshonk <mshonk@goldentech.com>
2016-06-15 13:56:53 -0400

    One more alteration

--- TestExample/TestExample/Form1.vb
+++ TestExample/TestExample/Form1.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("hi how are you")
+        MessageBox.Show("hi how are you?")
+        Dim answer = InputBox("Well?")
+        Label1.text = answer
     End Sub
 End Class
